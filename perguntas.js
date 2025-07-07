55caa5019834985ebc8f33d245b8a6651c9eff41
luana <luana.fatima.chaves@escola.pr.gov.br>
2025-07-07 11:40:27 -0300

    kjhhj

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    Biologia
+    Quantos meses dura uma gestação de uma égua?
+    Em média uma gestação dura 11 meses
+)
+
+criaCartao(
+    Séries
+    Quantas temporadas tem a série Ransom Canyon?
+    Tem uma temporada até o momento
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
